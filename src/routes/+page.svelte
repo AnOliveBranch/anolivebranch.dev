d5cd620ef3d4e4b3875651e5367c832344d56253
AnOliveBranch <liv@anolivebranch.dev>
2024-02-06 11:03:51 -0600

    Add template for homepage links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,36 @@
+<script>
+	import Link from './Link.svelte';
+	import github from '$lib/images/github-mark-white.svg';
+	import discord from '$lib/images/discord-mark-blue.svg';
+</script>
+
 <head>
 	<title>AnOliveBranch - Home</title>
 </head>
 
-<h1>Under Development</h1>
-<p>This website is under development. Please come back later.</p>
+<ul>
+	<li>
+		<Link img={github} link="https://github.com/AnOliveBranch" title="GitHub" />
+	</li>
+	<li>
+		<Link img={discord} link="https://discord.gg/BgZBg3hBDV" title="Discord" />
+	</li>
+	<li>
+		<Link img={github} link="https://github.com/AnOliveBranch" title="GitHub" />
+	</li>
+	<li>
+		<Link img={github} link="https://github.com/AnOliveBranch" title="GitHub" />
+	</li>
+	<li>
+		<Link img={github} link="https://github.com/AnOliveBranch" title="GitHub" />
+	</li>
+</ul>
+
+<style>
+	ul {
+		list-style: none;
+		display: flex;
+		justify-content: space-around;
+		padding: 0;
+	}
+</style>
---
+++ src/routes/Link.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let img = '';
+	export let link = '';
+	export let title = '';
+</script>
+
+<div>
+	<img src={img} alt={title} />
+	<a href={link} target="_blank">{title}</a>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		width: 200px;
+		border: solid rgb(100, 166, 241) 2px;
+	}
+
+	img {
+		border-bottom: solid rgb(100, 166, 241) 2px;
+		padding: 1em;
+		height: 150px;
+		object-fit: contain;
+	}
+
+	a {
+		text-decoration: none;
+		color: white;
+		text-align: center;
+		padding: 0.5em;
+		font-size: 1.5em;
+		transition: 0.5s;
+	}
+
+	a:hover {
+		background-color: rgb(120, 186, 255);
+	}
+</style>
