73bd960599eb7c8ae2c1a5cbd8a7a10d12c8d282
AnOliveBranch <liv@anolivebranch.dev>
2024-02-08 10:50:12 -0600

    Change link styling

--- src/routes/Link.svelte
+++ src/routes/Link.svelte
@@ -17,45 +17,82 @@
 
 {#if side == 'front'}
 	<div id="front">
-		<p>{title}</p>
+		<p id="title">{title}</p>
 		<img src={img} alt={title} />
-		<a href={link} target="_blank">Link</a>
-		<button on:click={showBack}>About</button>
+		<div class="bottom">
+			<a href={link} target="_blank">Link</a>
+			<button on:click={showBack}>About</button>
+		</div>
 	</div>
 {:else}
 	<div id="back">
-		<p>{title}</p>
+		<p id="title">{title}</p>
 		<p id="details">{details}</p>
-		<button on:click={showFront}>Back</button>
+		<button class="bottom" on:click={showFront}>Back</button>
 	</div>
 {/if}
 
 <style>
-	div {
+	div#front,
+	div#back {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
+		justify-content: space-around;
 		width: 200px;
-		border: solid rgb(100, 166, 241) 2px;
+		height: 300px;
+		background-color: rgb(40, 40, 40);
+		border-radius: 10%;
+		box-shadow: -3px 3px 5px 10px rgb(35, 35, 35);
 	}
 
-	img {
-		border-bottom: solid rgb(100, 166, 241) 2px;
-		padding: 1em;
+	img,
+	p#details {
+		padding: 0 1em 1em 1em;
+		margin: 0;
 		height: 150px;
 		object-fit: contain;
+	}
+
+	p,
+	a,
+	button {
+		font-family: Georgia, 'Times New Roman', Times, serif;
+		text-align: center;
 	}
 
 	a {
 		text-decoration: none;
 		color: white;
 		text-align: center;
-		padding: 0.5em;
 		font-size: 1.5em;
 		transition: 0.5s;
+		margin: 0;
+		padding: 0;
 	}
 
-	a:hover {
-		background-color: rgb(120, 186, 255);
+	button {
+		border: none;
+		background: none;
+		color: white;
+		font-size: 1.5em;
+		cursor: pointer;
+		margin: 0;
+		padding: 0;
+	}
+
+	.bottom {
+		display: flex;
+		justify-content: space-around;
+	}
+
+	.bottom a,
+	.bottom button,
+	button.bottom {
+		padding: 1em;
+	}
+
+	p#title {
+		margin: 0;
+		font-size: 1.5em;
 	}
 </style>
